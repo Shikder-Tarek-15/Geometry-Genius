5955389f89f7361b2389be0954e3e432a5f384fb
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-02-13 23:53:13 +0600

    Add file and some code

---
+++ script/calculate.js
@@ -0,0 +1,25 @@
+//Triangle Function
+function calculateTriangle(){
+    const triangleBase = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBase.value;
+    const base = parseFloat(triangleBaseText);
+
+
+    const triangleHeight = document.getElementById('triangle-height');
+    const height = parseFloat(triangleHeight.value);
+
+    const area = 0.5* base * height;
+    console.log(area);
+
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+}
+
+//Rectangle Function
+function calculateRectangle(){
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const width = parseFloat(rectangleWidth.value);
+
+    const rectangleLength = document.getElementById('rectangle-length');
+    const length = parseFloat(rectangleLength.value);
+}
