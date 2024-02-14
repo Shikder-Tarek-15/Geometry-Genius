b93a3de4eac5430d2d20361f9a7d817435b67e05
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-02-14 10:51:44 +0600

    add reusble function

--- script/calculate.js
+++ script/calculate.js
@@ -1,31 +1,37 @@
 //Triangle Function
 function calculateTriangle(){
-    const triangleBase = document.getElementById('triangle-base');
-    const triangleBaseText = triangleBase.value;
-    const base = parseFloat(triangleBaseText);
-
-
-    const triangleHeight = document.getElementById('triangle-height');
-    const height = parseFloat(triangleHeight.value);
+    const base = getInputValue('triangle-base');
+    const height = getInputValue('triangle-height');
 
     const area = 0.5* base * height;
     console.log(area);
 
-    const triangleAreaSpan = document.getElementById('triangle-area');
-    triangleAreaSpan.innerText = area;
+    const result = showResult('triangle-area', area);
 }
 
 //Rectangle Function
 function calculateRectangle(){
-    const rectangleWidth = document.getElementById('rectangle-width');
-    const width = parseFloat(rectangleWidth.value);
-
-    const rectangleLength = document.getElementById('rectangle-length');
-    const length = parseFloat(rectangleLength.value);
+    const width = getInputValue('rectangle-width');
+    const length = getInputValue('rectangle-length');
 
     const rectangleArea = width*length;
     console.log(rectangleArea);
 
-    const rectangleAreaSpan = document.getElementById('rectangle-area');
-    rectangleAreaSpan.innerText = rectangleArea;
+    const result = showResult('rectangle-area',rectangleArea);
+}
+
+
+
+//Get input value function
+function getInputValue(id){
+    const inputField = document.getElementById(id);
+    const inputValue = parseFloat(inputField.value);
+    return inputValue;
+}
+
+//show result at card
+function showResult(id,area){
+    const areaSpan = document.getElementById(id);
+    areaSpan.innerText = area;
+    console.log("I am Here")
 }
